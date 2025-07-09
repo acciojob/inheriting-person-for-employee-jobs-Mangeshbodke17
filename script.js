4f0f417d9d31451ec88b34a5b3b4dc79a358c90a
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-07-09 21:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-  // Call the parent constructor
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Set up inheritance from Person
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
